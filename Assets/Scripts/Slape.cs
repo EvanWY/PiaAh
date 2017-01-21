44bf86cead5877aa51039d1c8a8c873ff08a20a5
yxwang0603 <yxwang0603@gmail.com>
2017-01-21 17:04:39 -0500

    slape

---
+++ Assets/Scripts/Slape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slape : MonoBehaviour {
+
+    public float radius;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D[] hits;
+            hits = Physics2D.CircleCastAll(Input.mousePosition, radius, Camera.main.transform.forward);
+            foreach(RaycastHit2D hit in hits)
+            {
+                var state = hit.collider.GetComponent<ManState>();
+                //if( state == ManState.AudienceState.Sleep)
+               // {
+
+                //}
+            }
+        }
+	}
+}
