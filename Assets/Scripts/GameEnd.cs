b2f746de35a28e9d7f22b15304878d4a2be170c2
yxwang0603 <yxwang0603@gmail.com>
2017-01-22 02:20:30 -0500

    1

--- Assets/Scripts/GameEnd.cs
+++ Assets/Scripts/GameEnd.cs
@@ -9,13 +9,29 @@ public class GameEnd : MonoBehaviour {
     public GameObject gameOverPanel;
     public Text score;
     public Sprite[] results;
+    public int threshold;
     private int finalScore;
+    private int totalMiss;
+    private Image im;
 	// Use this for initialization
 
 	void Start () {
+        im = GetComponent<Image>();
         finalScore = Score.currentScore;
         score.text = finalScore.ToString();
+        totalMiss = Health.missNum;
 
-        if()
+        if( totalMiss == 0)
+        {
+            im.sprite = results[0];
+        }
+        if( 0<totalMiss && totalMiss < threshold)
+        {
+            im.sprite = results[1];
+        }
+        if( threshold < totalMiss)
+        {
+            im.sprite = results[2];
+        }
 	}
 }
