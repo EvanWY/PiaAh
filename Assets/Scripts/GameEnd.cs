c8c95a326f01a512c1b17728b7456e1706295d0b
yxwang0603 <yxwang0603@gmail.com>
2017-01-21 20:03:30 -0500

    32

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour {
+
+    public GameObject endingPanel;
+    public Text finalScore;
+
+    public Text perfectText;
+    public Text loseText;
+	// Use this for initialization
+	void Start () {
+
+        endingPanel.SetActive(true);
+        finalScore.text = Score.currentScore.ToString();
+
+        if (Health.currentHealth <= 0)
+        {
+            loseText.enabled = true;
+        }
+        else
+        {
+            perfectText.enabled = true;
+        }
+	}
+}
